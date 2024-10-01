47fbc221c4e326eb3819fd22b4e75d4cb7bdc069
balta <baltaasanchez@gmail.com>
2024-10-01 07:00:43 -0300

    Delete files and add NgFor to Imports

---
+++ src/app/services/load-scripts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable,Renderer2, RendererFactory2  } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadScriptsService {
+
+  private renderer: Renderer2;
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
+
+
+}
