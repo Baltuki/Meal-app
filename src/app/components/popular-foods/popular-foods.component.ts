cdf6afe4cfcbda6afbd8584604d382be2a600812
balta <baltaasanchez@gmail.com>
2024-10-03 18:47:21 -0300

    update

--- src/app/components/popular-foods/popular-foods.component.ts
+++ src/app/components/popular-foods/popular-foods.component.ts
@@ -29,7 +29,30 @@ export class PopularFoodsComponent implements OnInit{
     autoplaySpeed: 3000,
     dots: true,
     infinite: true,
-    arrows: true
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024, 
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480, 
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   };
 
   ngOnInit(): void {
